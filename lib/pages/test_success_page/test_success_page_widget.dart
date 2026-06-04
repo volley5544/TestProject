417bd91866d657032e9f1622fb245570a1a5fb72
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-06-04 08:25:33 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/test_success_page/test_success_page_widget.dart
+++ lib/pages/test_success_page/test_success_page_widget.dart
@@ -205,6 +205,25 @@ class _TestSuccessPageWidgetState extends State<TestSuccessPageWidget> {
                           ),
                     ),
                   ),
+                  Text(
+                    'debug : ${FFAppState().debugText}',
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          font: GoogleFonts.inter(
+                            fontWeight: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .fontWeight,
+                            fontStyle: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .fontStyle,
+                          ),
+                          letterSpacing: 0.0,
+                          fontWeight: FlutterFlowTheme.of(context)
+                              .bodyMedium
+                              .fontWeight,
+                          fontStyle:
+                              FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                        ),
+                  ),
                 ].addToStart(SizedBox(height: 12.0)),
               ),
             ),
