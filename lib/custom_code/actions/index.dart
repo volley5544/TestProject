b064c53faaf8c20e63ecb9ab69083ae190aa94dc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-06-10 03:16:37 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -1,2 +1,4 @@
 export '/custom_code/actions/open_third_party.dart' show openThirdParty;
 export '/custom_code/actions/open_in_app_browser.dart' show openInAppBrowser;
+export '/custom_code/actions/url_launcher_external_app.dart'
+    show urlLauncherExternalApp;
---
+++ lib/custom_code/actions/url_launcher_external_app.dart
@@ -0,0 +1,18 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:url_launcher/url_launcher.dart';
+
+Future urlLauncherExternalApp(String? urlLink) async {
+  // Add your function code here!
+  await launchUrl(Uri.parse(urlLink!), mode: LaunchMode.externalApplication);
+}
